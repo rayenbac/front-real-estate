ca86c40fe1e0e4e5a213453dacc6c13868e0a204
rayenbac <rayenbac1@gmail.com>
2025-03-09 22:32:20 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { 
+    path: 'home', 
+    loadComponent: () => import('./front-office/front-office.component').then(m => m.FrontOfficeComponent) 
+  },
+  { 
+    path: 'admin', 
+    loadComponent: () => import('./back-office/back-office.component').then(m => m.BackOfficeComponent) 
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
---
+++ src/app/back-office/back-office.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-back-office',
+  standalone: true,
+  imports: [],
+  templateUrl: './back-office.component.html',
+  styleUrl: './back-office.component.css'
+})
+export class BackOfficeComponent {
+
+}
