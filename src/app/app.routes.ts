7aca943b06b2c52095eea4deab50a1c513e9e5cd
rayenbac <rayenbac1@gmail.com>
2025-03-10 13:37:44 +0100

    template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,20 @@
 import { Routes } from '@angular/router';
-
+import { LayoutComponent } from './front-office/layout/layout.component';
 
 export const routes: Routes = [
-  { 
-    path: 'home', 
-    loadComponent: () => import('./front-office/front-office.component').then(m => m.FrontOfficeComponent) 
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { 
+        path: 'home', 
+        loadComponent: () => import('./front-office/layout/layout.component').then(m => m.LayoutComponent) 
+      },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+    ]
   },
   { 
     path: 'admin', 
     loadComponent: () => import('./back-office/back-office.component').then(m => m.BackOfficeComponent) 
   },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
